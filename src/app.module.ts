eaf1ab44c1ce7e1a34802bb78a9a829f6e95d89f
kstobiecki <stobiecki.kamil@gmail.com>
2021-09-28 20:39:33 +0200

    Add mongoose and connect to mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/bash-cmd')],
 })
 export class AppModule {}
